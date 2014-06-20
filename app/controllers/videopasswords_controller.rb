be40f8c8bb7cfee23ca9f4ba1f28d1d74f7173bd
Mark Ratliff <ratliff@princeton.edu>
2014-06-20 13:19:02 -0400

    Include form validation.

--- app/controllers/videopasswords_controller.rb
+++ app/controllers/videopasswords_controller.rb
@@ -1,31 +1,38 @@
 class VideopasswordsController < ApplicationController
 
   def index
+    @video = Video.new()
   end
 
   def insert
+
+#    Show params values when debugging
 #    render plain: params[:video].inspect
     @video = Video.new(video_params)
 
     # Encrypt the password before saving in DB
     salt = BCrypt::Engine.generate_salt
-    encrypted_password = BCrypt::Engine.hash_secret(@video.password_hash, salt)
-
-    @video.password_hash = encrypted_password
-
     @video.password_salt = salt
 
-    @video.save
+    # If no password was submitted, then leave blank
+    if !@video.password_hash.blank?
+      encrypted_password = BCrypt::Engine.hash_secret(@video.password_hash, salt)
 
-    redirect_to "/videopasswords/confirm"
+      @video.password_hash = encrypted_password
+    end 
+
+    #  Attempt to save the video.  If save fails, return to index page
+    if @video.save
+      redirect_to "/videopasswords/confirm"
+    else
+      render "index"
+    end
+
   end
 
   def showvideo
 
     # If password in post data is correct, then show video
-
-
-    # Else, presnet form
 
   end
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,13 @@
 class Video < ActiveRecord::Base
+
+  validates_presence_of :entry_id
+  # Make sure that the entry_id contains an underscore
+  validates_format_of :entry_id, :with => /.+_.+/, :message => "entry_id should contain an underscore character"
+
+  validates_presence_of :uiconf_id
+
+  validates_presence_of :password_hash
+  validates_length_of :password_hash, :minimum => 8
+
+  validates_presence_of :password_salt
 end
